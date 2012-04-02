66b4cd9bbd2d0aef60e49a2ef4bc204a627ab240
David Barragan <david.barragan@kaleidos.net>
2012-04-02 19:23:25 +0200

    Added new functionality: now we can prioritize all the items, the featured and not featured ones

--- highlighter/models.py
+++ highlighter/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 from .settings import *
 
-HIGHLIGHT_PRIORITY_CHOICES = [ (x,x) for x in range(*HIGHLIGHTER_PRIORITY_RANGE) ]
+PRIORITY_CHOICES = [ (x,x) for x in range(*HIGHLIGHTER_PRIORITY_RANGE) ]
+
 
 class HighlightedManager(models.Manager):
     def get_query_set(self):
-        return super(HighlightedManager, self).get_query_set().filter(highlighted=True).order_by('-highlight_priority')
+        return super(HighlightedManager, self).get_query_set().filter(is_highlighted=True).order_by('-priority')
+
+
+class PrioritizedManager(models.Manager):
+    def get_query_set(self):
+        return super(PrioritizedManager, self).get_query_set().all().order_by('-is_highlighted','-priority')
+
 
 class HighlighterMixin(models.Model):
-    highlighted = models.BooleanField()
-    highlight_priority = models.IntegerField(null=True, blank=True,
-                choices=HIGHLIGHT_PRIORITY_CHOICES)
+    is_highlighted = models.BooleanField()
+    priority = models.IntegerField(null=True, blank=True,
+                choices=PRIORITY_CHOICES)
     objects = models.Manager()
-    highlighteds = HighlightedManager()
+    highlighted_objects = HighlightedManager()
+    prioritized_objects = PrioritizedManager()
 
     class Meta:
         abstract = True
--- highlighter/tests/__init__.py
+++ highlighter/tests/__init__.py
@@ -3,13 +3,17 @@ from .models import TestModel
 
 class HighlighterTest(TestCase):
     def setUp(self):
-        TestModel(name="test1", highlighted=True, highlight_priority=10).save()
-        TestModel(name="test2", highlighted=True, highlight_priority=20).save()
-        TestModel(name="test3", highlighted=False, highlight_priority=30).save()
-        TestModel(name="test4", highlighted=False, highlight_priority=40).save()
-        TestModel(name="test5", highlighted=True, highlight_priority=60).save()
-        TestModel(name="test6", highlighted=True, highlight_priority=50).save()
+        TestModel.objects.create(name="test1", is_highlighted=True,  priority=10)
+        TestModel.objects.create(name="test2", is_highlighted=True,  priority=20)
+        TestModel.objects.create(name="test3", is_highlighted=False, priority=30)
+        TestModel.objects.create(name="test4", is_highlighted=False, priority=40)
+        TestModel.objects.create(name="test5", is_highlighted=True,  priority=60)
+        TestModel.objects.create(name="test6", is_highlighted=True,  priority=50)
         
-    def test_list_highlighteds(self):
-        highlighteds = [ x.name for x in TestModel.get_highlighteds() ]
-        self.assertEqual(highlighteds, [ "test5", "test6", "test2", "test1" ])
+    def test_list_highlightes(self):
+        highlighted_list = [ x.name for x in TestModel.highlighted_objects.all() ]
+        self.assertEqual(highlighted_list, ["test5", "test6", "test2", "test1"])
+    
+    def test_list_prioritizes(self):
+        prioritized_list = [ x.name for x in TestModel.prioritized_objects.all() ]
+        self.assertEqual(prioritized_list, ["test5", "test6", "test2", "test1", "test4", "test3"])
