62f2357bc455ac314179ce2b273f0dd63be0bed0
David Barragan <david.barragan@kaleidos.net>
2012-04-03 10:03:35 +0200

    Changed prioty atribute name to highlight_priority

--- highlighter/tests/__init__.py
+++ highlighter/tests/__init__.py
@@ -3,12 +3,12 @@ from .models import TestModel
 
 class HighlighterTest(TestCase):
     def setUp(self):
-        TestModel.objects.create(name="test1", is_highlighted=True,  priority=10)
-        TestModel.objects.create(name="test2", is_highlighted=True,  priority=20)
-        TestModel.objects.create(name="test3", is_highlighted=False, priority=30)
-        TestModel.objects.create(name="test4", is_highlighted=False, priority=40)
-        TestModel.objects.create(name="test5", is_highlighted=True,  priority=60)
-        TestModel.objects.create(name="test6", is_highlighted=True,  priority=50)
+        TestModel.objects.create(name="test1", is_highlighted=True,  highlight_priority=10)
+        TestModel.objects.create(name="test2", is_highlighted=True,  highlight_priority=20)
+        TestModel.objects.create(name="test3", is_highlighted=False, highlight_priority=30)
+        TestModel.objects.create(name="test4", is_highlighted=False, highlight_priority=40)
+        TestModel.objects.create(name="test5", is_highlighted=True,  highlight_priority=60)
+        TestModel.objects.create(name="test6", is_highlighted=True,  highlight_priority=50)
         
     def test_list_highlightes(self):
         highlighted_list = [ x.name for x in TestModel.highlighted_objects.all() ]
